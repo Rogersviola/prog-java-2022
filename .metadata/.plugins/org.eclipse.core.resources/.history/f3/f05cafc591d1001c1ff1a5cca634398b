97cd13a5b9f4a0dd8284fc8694c625784311a908
Rogers Viola <violaengenharia@gmail.com>
2022-05-11 22:24:07 -0300

    aula atualizada 16

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/f3/f05cafc591d1001c1ff1a5cca634398b
@@ -0,0 +1,10 @@
+package br.com.senaisp.aula15;
+
+public class ExemploCalculoTemperatura {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- progjava2022/src/br/com/senaisp/aula14/ExemploMamiferoEncapsulado.java
+++ progjava2022/src/br/com/senaisp/aula14/ExemploMamiferoEncapsulado.java
@@ -9,22 +9,48 @@ public class ExemploMamiferoEncapsulado {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MamiferosEncapsulada humano = new MamiferosEncapsulada();
-		Scanner sc = new Scanner (System.in);
-		System.out.println("Digite a cor da pele do humano:");
-		humano.setPele(sc.nextLine());
-		System.out.println("Digite a cor da olhos do humano:");
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("Digite a cor da pele do humano:");
+			try {
+				humano.setPele( sc.nextLine() );
+				break;
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				System.out.println(e1.getMessage());
+			}
+		} while(true);
+		do {
+			System.out.println("Digite a cor dos olhos do humano:");
+			try {
+				humano.setOlhos( sc.nextLine() );
+				break;
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println(e.getMessage());
+			}
+		} while(true);
 		
-		System.out.println("Digite a tamanho da boca do humano:");
-		humano.setBoca(sc.nextLine());
-		System.out.println("Digite o peso do humano:");
-		humano.setPeso(sc.nextDouble());
+		System.out.println("Digite o tamanho da boca do humano:");
+		humano.setBoca( sc.nextLine() );
 		
+		do {
+			System.out.println("Digite o peso do humano:");
+			try {
+				humano.setPeso( sc.nextDouble() );
+				break;
+			} catch(Exception e2) {
+				System.out.println("Valor inválido!");
+				sc.nextLine();
+			}
+		} while(true);
+
 		//Mostrando os dados cadastrados
 		System.out.println("Pele digitada:" + humano.getPele());
-		System.out.println("Olhos digitada:" + humano.getOlhos());
-		System.out.println("Boca digitada:" + humano.getBoca());
-		System.out.println("Peso digitada:" + humano.getPeso());
-
+		System.out.println("Olhos digitado:" + humano.getOlhos());
+		System.out.println("Boca digitado:" + humano.getBoca());
+		System.out.println("Peso digitado:" + humano.getPeso());
+		sc.close();
 	}
 
 }
