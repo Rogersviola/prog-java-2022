8592e5dbea6bb53cc8ba250f7bc478ef076040d4
Rogers Viola <violaengenharia@gmail.com>
2022-05-25 22:29:24 -0300

    material aula 22 projeto pessoal

--- ProjetoPessoalSistemaCaixa/src/br/com/rogers/aula/classes/ConexaoFake.java
+++ ProjetoPessoalSistemaCaixa/src/br/com/rogers/aula/classes/ConexaoFake.java
@@ -1,5 +1,12 @@
 package br.com.rogers.aula.classes;
 
 public class ConexaoFake {
+	private static ConexaoFake instancia;
+
+	public static ConexaoFake getInstance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	private
 
 }
